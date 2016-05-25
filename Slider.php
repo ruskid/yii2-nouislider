6375b41c840e7d22b93cbdb1a0b077f259d7ec1e
demin <demin@demin-X555LAB>
2016-05-25 18:15:47 +0200

    added updateJsExpression

--- Slider.php
+++ Slider.php
@@ -12,6 +12,7 @@ use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 use ruskid\nouislider\SliderAsset;
+use yii\web\JsExpression;
 
 /**
  * Yii noUiSlider input widget
@@ -20,105 +21,122 @@ use ruskid\nouislider\SliderAsset;
  */
 class Slider extends InputWidget {
 
-    /**
-     * @see http://refreshless.com/nouislider/
-     * @var array noUiSlider config options in array format
-     */
-    public $pluginOptions;
+	/**
+	 * @see http://refreshless.com/nouislider/
+	 * @var array noUiSlider config options in array format
+	 */
+	public $pluginOptions;
 
-    /**
-     * @var string Slider tag
-     */
-    public $sliderTag = 'div';
+	/**
+	 * @var string Slider tag
+	 */
+	public $sliderTag = 'div';
 
-    /**
-     * @var array Slider div options. Can be used to customize sliders
-     */
-    public $sliderTagOptions = ['class' => 'ruskid-nouislider'];
+	/**
+	 * @var array Slider div options. Can be used to customize sliders
+	 */
+	public $sliderTagOptions = ['class' => 'ruskid-nouislider'];
 
-    /**
-     * @var string Id of container with lower value selected
-     */
-    public $lowerValueContainerId;
+	/**
+	 * @var string Id of container with lower value selected
+	 */
+	public $lowerValueContainerId;
 
-    /**
-     * @var string Id of container with higher value selected
-     */
-    public $upperValueContainerId;
+	/**
+	 * @var string Id of container with higher value selected
+	 */
+	public $upperValueContainerId;
 
-    /**
-     * @var string Character separating values in hidden input. Used on 2 handle case.
-     */
-    public $valueSeparator = '|';
+	/**
+	 * @var JsExpression
+	 */
+	public $updateSliderEventExpression;
 
-    /**
-     * @var string
-     */
-    const SLIDER_ID_POSTFIX = '_nouislider';
+	/**
+	 * @var string Character separating values in hidden input. Used on 2 handle case.
+	 */
+	public $valueSeparator = '|';
 
-    /**
-     * @var boolean If slider has 1 or 2 handles
-     */
-    private $_twoHandleCase = false;
+	/**
+	 * @var string
+	 */
+	const SLIDER_ID_POSTFIX = '_nouislider';
 
-    /**
-     * Init slider. 
-     */
-    public function init() {
-        parent::init(); //Check if sldier has 2 handles
-        $this->_twoHandleCase = count($this->pluginOptions['start']) === 2;
-    }
+	/**
+	 * @var boolean If slider has 1 or 2 handles
+	 */
+	private $_twoHandleCase = false;
 
-    /**
-     * @inheritdoc
-     */
-    public function run() {
-        $view = $this->getView();
-        SliderAsset::register($view);
+	/**
+	 * Init slider. 
+	 */
+	public function init() {
+		parent::init(); //Check if sldier has 2 handles
+		$this->_twoHandleCase = count($this->pluginOptions['start']) === 2;
 
-        $this->renderSlider();
-        $this->renderInput();
-        $this->registerUpdateSliderJs();
-    }
+		if (!$this->updateSliderEventExpression) {
+			$this->updateSliderEventExpression = $this->loadDefaultUpdateSliderExpression();
+		}
+	}
 
-    /**
-     * Will render slider div
-     */
-    private function renderSlider() {
-        $view = $this->getView();
-        $sliderId = $this->constructSliderId();
-        $jsOptions = Json::encode($this->pluginOptions);
-        $js = "var $sliderId = document.getElementById('$sliderId'); noUiSlider.create($sliderId, $jsOptions);";
-        $view->registerJs($js);
+	/**
+	 * @inheritdoc
+	 */
+	public function run() {
+		$view = $this->getView();
+		SliderAsset::register($view);
 
-        $this->sliderTagOptions['id'] = $sliderId;
-        echo Html::tag($this->sliderTag, '', $this->sliderTagOptions);
-    }
+		$this->renderSlider();
+		$this->renderInput();
+		$this->registerUpdateSliderJs();
+	}
 
-    /**
-     * @return string Get constructed slider id
-     */
-    private function constructSliderId() {
-        return $this->id . self::SLIDER_ID_POSTFIX;
-    }
+	/**
+	 * Will render slider div
+	 */
+	private function renderSlider() {
+		$view = $this->getView();
+		$sliderId = $this->constructSliderId();
+		$jsOptions = Json::encode($this->pluginOptions);
+		$js = "var $sliderId = document.getElementById('$sliderId'); noUiSlider.create($sliderId, $jsOptions);";
+		$view->registerJs($js);
 
-    /**
-     * Will render input for the slider value
-     */
-    private function renderInput() {
-        echo $this->hasModel() ?
-                Html::activeHiddenInput($this->model, $this->attribute, $this->options) :
-                Html::hiddenInput($this->name, $this->value, $this->options);
-    }
+		$this->sliderTagOptions['id'] = $sliderId;
+		echo Html::tag($this->sliderTag, '', $this->sliderTagOptions);
+	}
 
-    /**
-     * Register Slider Update JS Handler. 
-     */
-    public function registerUpdateSliderJs() {
-        $sliderId = $this->constructSliderId();
-        $inputId = $this->options['id'];
+	/**
+	 * @return string Get constructed slider id
+	 */
+	private function constructSliderId() {
+		return $this->id . self::SLIDER_ID_POSTFIX;
+	}
 
-        $js = "$sliderId.noUiSlider.on('update', function( values, handle ) {
+	/**
+	 * Will render input for the slider value
+	 */
+	private function renderInput() {
+		echo $this->hasModel() ?
+				Html::activeHiddenInput($this->model, $this->attribute, $this->options) :
+				Html::hiddenInput($this->name, $this->value, $this->options);
+	}
+
+	/**
+	 * Register Slider Update Event 
+	 */
+	private function registerUpdateSliderJs() {
+		$sliderId = $this->constructSliderId();
+		$js = "$sliderId.noUiSlider.on('update', $this->updateSliderEventExpression)";
+		$this->getView()->registerJs($js);
+	}
+
+	/**
+	 * Load default slider update js expression
+	 * @return JsExpression
+	 */
+	private function loadDefaultUpdateSliderExpression() {
+		$inputId = $this->options['id'];
+		return new JsExpression("function( values, handle ) {
             if('$this->lowerValueContainerId'){
                 document.getElementById('$this->lowerValueContainerId').innerHTML = values[0];
             }
@@ -136,18 +154,7 @@ class Slider extends InputWidget {
             }else{
                 input.value = values[0];
             }
-        });";
-        
-        $this->getView()->registerJs($js);
-    }
-    
-    /**
-     * If input updated. set slider's values.
-     */
-    public function registerUpdateInputJs(){
-       /* "document.getElementById('$inputId').addEventListener('change', function(){
-            $sliderId.noUiSlider.set([this.value]);
-        });";*/
-    }
+        }");
+	}
 
 }
